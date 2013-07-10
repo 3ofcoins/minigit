9f02ceb6813ec83ad393d67d5c0208ded75ace3d
Maciej Pasternacki <maciej@3ofcoins.net>
2013-07-10 16:16:48 +0200

    Reformat tabs -> spaces

--- spec/minigit_hash_access_spec.rb
+++ spec/minigit_hash_access_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe MiniGit do
-	let(:git) { MiniGit::new }
+  let(:git) { MiniGit::new }
 
-	describe '#[]' do
-		it 'returns nil if the passed in attribute has no value' do
-			assert { git['foo.bar'] == nil }
-			assert { MiniGit['foo.bar'] == nil }
-		end
-	end
+  describe '#[]' do
+    it 'returns nil if the passed in attribute has no value' do
+      assert { git['foo.bar'] == nil }
+      assert { MiniGit['foo.bar'] == nil }
+    end
+  end
 
-	describe '#[]=' do
-		it 'assigns value to a git config attribute' do
-			git['bar.baz'] = 'foo'
-			MiniGit['bar.yyz'] = 'yyz'
-			assert { git['bar.baz'] == "foo\n" }
-			assert { MiniGit['bar.yyz'] == "yyz\n" }
-		end
-	end
+  describe '#[]=' do
+    it 'assigns value to a git config attribute' do
+      git['bar.baz'] = 'foo'
+      MiniGit['bar.yyz'] = 'yyz'
+      assert { git['bar.baz'] == "foo\n" }
+      assert { MiniGit['bar.yyz'] == "yyz\n" }
+    end
+  end
 
 end
